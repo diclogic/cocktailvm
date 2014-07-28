2c59130c43a9e3836e0c227aa582659e9fd67ceb
diclogic <diclogic@gmail.com>
2014-07-28 23:33:13 +0200

    -F: unhandled exception log

--- src/Launcher/Program.cs
+++ src/Launcher/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Core.Aux.System;
+using System.Threading;
 
 namespace Launcher
 {
@@ -13,9 +15,26 @@ namespace Launcher
         [STAThread]
         static void Main()
         {
+			Log.Info("SYSTEM", "Application started!");
+			//Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException, false);
+			//Application.ThreadException += (_, e) => LogException(e.Exception);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+			try
+			{
+				Application.Run(new Form1());
+			}
+			catch (Exception ex)
+			{
+				LogException(ex);
+			}
         }
+
+		static void LogException(Exception ex)
+		{
+			System.Console.WriteLine("Unhandled Exception: {0}", ex.ToString());
+			Log.Info("SYSTEM", "Unhandled Exception: {0}", ex.ToString());
+		}
     }
 }
